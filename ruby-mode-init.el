ce876a9bbca86449e8387836e3162fa457df10d9
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2011-03-21 18:44:50 +0300

    Added ruby-mode exported from trunk

--- ruby-mode-init.el
+++ ruby-mode-init.el
@@ -1,10 +1,18 @@
+(add-third-party-dir "bundled/ruby-mode")
+
+(autoload 'run-ruby "inf-ruby"
+  "Run inferior Ruby interpreter")
+(autoload 'inf-ruby-keys "inf-ruby" 
+  "Add inf-ruby keys to ruby mode map")
+
 (defun ruby-mode-keys ()
   (define-key ruby-mode-map "\r" 'reindent-then-newline-and-indent)
   (define-key ruby-mode-map "\C-\M-j" 'insert-new-line)
   (define-key ruby-mode-map "\M-\r" 'insert-new-line))
 
 (defun customize-ruby-mode ()
-  (ruby-mode-keys))
+  (ruby-mode-keys)
+  (inf-ruby-keys))
 
 (add-hook 'ruby-mode-hook 'customize-ruby-mode)
 
