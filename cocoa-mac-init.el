38c6e3f227abd09b9a98984fc08060056b26dea3
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-04-22 10:02:44 +0400

    Added init file for Mac

---
+++ cocoa-mac-init.el
@@ -0,0 +1,6 @@
+;; Here I put some Mac-specific Emacs initialization code
+
+(set-face-font 'default "-apple-monaco-medium-r-normal--15-140-72-72-m-140-mac-roman")
+
+(blink-cursor-mode t)
+(setq ns-command-modifier 'meta)
