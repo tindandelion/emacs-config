e5db17d6882b6cd44c322b70ab04e59cf9c14469
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-04-22 10:16:57 +0400

    Look and feel now check if tool-bar-mode and scroll-bar-mode present before calling them: needed for command-line emacs in MacOS

--- look-and-feel.el
+++ look-and-feel.el
@@ -1,7 +1,11 @@
 ;; Turn off annoying visual and sound stuff
 (setq ring-bell-function 'ignore)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
 (menu-bar-mode -1)
 (setq inhibit-startup-message t)
 (setq transient-mark-mode t)
