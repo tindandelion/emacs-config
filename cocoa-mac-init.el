6c5e870bd95216c0d2ba17fd9492e2c6e97cd863
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-04-24 09:04:13 +0400

    For Mac OS, maximize the frame at opening Emacs

--- cocoa-mac-init.el
+++ cocoa-mac-init.el
@@ -4,3 +4,8 @@
 
 (blink-cursor-mode t)
 (setq ns-command-modifier 'meta)
+
+;; Maximizing the frame to fit the full screen
+(set-frame-position (selected-frame) 0 0)
+(set-frame-size (selected-frame) 1000 1000)
+
