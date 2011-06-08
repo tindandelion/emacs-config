7e1a12fe041f16a1f83bcc4b344326baeb8bbe2f
moshse <sergey.moshnikov@f-secure.com>
2011-06-08 16:30:45 +0400

    Disabled ropemacs temporarily, as it requires some extra work

--- python-mode-init.el
+++ python-mode-init.el
@@ -1,9 +1,10 @@
 (add-third-party-dir "bundled/pymacs")
-(require 'pymacs)
+;(require 'pymacs)
 
-(add-to-list 'pymacs-load-path (third-party-dir "bundled/rope"))
-(add-to-list 'pymacs-load-path (third-party-dir "bundled/ropemacs"))
-; (pymacs-load "ropemacs" "rope-")
+;(add-to-list 'pymacs-load-path (third-party-dir "bundled/rope"))
+;(add-to-list 'pymacs-load-path (third-party-dir "bundled/ropemacs"))
+;(add-to-list 'pymacs-load-path (third-party-dir "bundled/ropemode"))
+;(pymacs-load "ropemacs" "rope-")
 
 
 
