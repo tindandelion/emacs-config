caaff5acfc68a9df02e5014f8b24142fb5c22ebb
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2011-09-06 19:31:41 +0400

    In Python mode, enable flymake mode with pyflakes

--- python-mode-init.el
+++ python-mode-init.el
@@ -6,8 +6,27 @@
 ;(add-to-list 'pymacs-load-path (third-party-dir "bundled/ropemode"))
 ;(pymacs-load "ropemacs" "rope-")
 
+(defun install-keybindings ()
+  (define-key python-mode-map "\C-m" 'newline-and-indent))
+
+(defun enable-pyflakes ()
+  (when (load "flymake" t) 
+    (defun flymake-pyflakes-init () 
+      ;; Make sure it's not a remote buffer or flymake would not work
+      (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+	(let* ((temp-file (flymake-init-create-temp-buffer-copy 
+			   'flymake-create-temp-inplace)) 
+	       (local-file (file-relative-name 
+			    temp-file 
+			    (file-name-directory buffer-file-name)))) 
+	  (list "pyflakes" (list local-file)))))
+    (add-to-list 'flymake-allowed-file-name-masks 
+		 '("\\.py\\'" flymake-pyflakes-init))))
+
 (add-hook 'python-mode-hook
 	  '(lambda ()
-	     (define-key python-mode-map "\C-m" 'newline-and-indent)))
+	     (enable-pyflakes)
+	     (flymake-mode t)
+	     (install-keybindings)))
 
 
