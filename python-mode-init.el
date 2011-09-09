3809c579dc24b94ca6697aef0b77018f6f0de740
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-09-09 21:23:06 +0400

    flymake now creates temp files in a temp directory, so it does not clash with auto test runner

--- python-mode-init.el
+++ python-mode-init.el
@@ -9,13 +9,16 @@
 (defun install-keybindings ()
   (define-key python-mode-map "\C-m" 'newline-and-indent))
 
+(defun flymake-create-temp-in-system-tempdir (filename prefix)
+  (make-temp-file (or prefix "flymake")))
+
 (defun enable-pyflakes ()
   (when (load "flymake" t) 
     (defun flymake-pyflakes-init () 
       ;; Make sure it's not a remote buffer or flymake would not work
       (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
 	(let* ((temp-file (flymake-init-create-temp-buffer-copy 
-			   'flymake-create-temp-inplace)) 
+			   'flymake-create-temp-in-system-tempdir)) 
 	       (local-file (file-relative-name 
 			    temp-file 
 			    (file-name-directory buffer-file-name)))) 
