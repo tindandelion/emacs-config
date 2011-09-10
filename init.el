13c6b631807c286d99cf61394781bcabce87b3d1
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-09-10 12:29:25 +0400

    Root .emacs.d is now added to load path and all customization files are loaded with load-library function

--- init.el
+++ init.el
@@ -1,42 +1,42 @@
-;; Load Path
-(setq dotfiles-dir (file-name-directory 
-		    (or (buffer-file-name) load-file-name)))
+;; --- Building the load path 
+;; Adding the root emacs config directory to the load path
+(setq root-config-dir (file-name-directory 
+		       (or (buffer-file-name) load-file-name)))
 
-(setq third-party (concat dotfiles-dir "third-party/"))
-(add-to-list 'load-path third-party)
+(add-to-list 'load-path root-config-dir)
 
-(defun third-party-dir (dirname)
-  (concat third-party dirname))
+(setq third-party-dir
+      (file-name-as-directory (concat root-config-dir "third-party")))
+(add-to-list 'load-path third-party-dir)
+
+(defun make-third-party-dir (dirname)
+  (concat third-party-dir dirname))
 
 (defun add-third-party-dir (dirname)
-  (let ((dir (third-party-dir dirname)))
+  (let ((dir (make-third-party-dir dirname)))
     (add-to-list 'load-path dir)
     dir))
-
-(defun load-customization-file (filename)
-  (load-file (concat dotfiles-dir filename)))
 
 (add-third-party-dir "bundled")
 
 ;; Customizations for different modules
-(load-customization-file "look-and-feel.el")
-(load-customization-file "color-theme-init.el")
-(load-customization-file "magit-init.el")
-(load-customization-file "auto-completion-init.el")
-;; (load-customization-file "tab-completion.el")
-(load-customization-file "ruby-mode-init.el")
-(load-customization-file "python-mode-init.el")
-(load-customization-file "text-mode-init.el")
-(load-customization-file "org-mode-init.el")
+(load-library "look-and-feel")
+(load-library "color-theme-init")
+(load-library "magit-init")
+(load-library "auto-completion-init")
+(load-library "ruby-mode-init")
+(load-library "python-mode-init")
+(load-library "text-mode-init")
+(load-library "org-mode-init")
 ;; Platform-dependent 
 (setq os-init-file 
-      (cond ((eq window-system 'mac) "carbon-mac-init.el")
-	    ((eq window-system 'ns)  "cocoa-mac-init.el")
-	    ((eq window-system 'w32) "win-init.el")
-	    ((eq window-system 'x) "linux-init.el")))
+      (cond ((eq window-system 'mac) "carbon-mac-init")
+	    ((eq window-system 'ns)  "cocoa-mac-init")
+	    ((eq window-system 'w32) "win-init")
+	    ((eq window-system 'x) "linux-init")))
 
 (if os-init-file
-  (load-file (concat dotfiles-dir os-init-file)))
+  (load-library os-init-file))
 
 (put 'erase-buffer 'disabled nil)
 (custom-set-variables
