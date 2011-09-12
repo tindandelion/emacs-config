553ac5825f72a12f63ad19785611c91458ccdeff
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2011-09-12 12:03:34 +0400

    Turned off wrapping long lines

--- init.el
+++ init.el
@@ -45,7 +45,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(fill-column 80))
+ '(fill-column 80)
+ '(truncate-lines t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
