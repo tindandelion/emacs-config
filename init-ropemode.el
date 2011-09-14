2cd60ac708c7885f85e4beaff72d907305c330bc
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-09-14 12:40:15 +0400

    Pymacs init

--- init-ropemode.el
+++ init-ropemode.el
@@ -1,5 +1,2 @@
-(setq pymacs-load-path 
-      (concat third-party-dir "bundled/python/lib/python"))
-
 (require 'pymacs)
 (pymacs-load "ropemacs" "rope-")
--- pymacs-init.el
+++ pymacs-init.el
@@ -3,6 +3,12 @@
        third-party-dir 
        "bundled/python/pymacs/lib/python"))
 (add-bundled-library-dir "python")
+
+(setq pymacs-load-path 
+      '("/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python/rope-0.9.3-py2.6.egg" 
+	"/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python/ropemacs-0.6-py2.6.egg" 
+	"/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python/ropemode-0.1_rc2-py2.6.egg"))
+
 (autoload 'pymacs-apply "pymacs")
 (autoload 'pymacs-call "pymacs")
 (autoload 'pymacs-eval "pymacs" nil t)
