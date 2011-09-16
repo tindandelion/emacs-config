1c2d00ffbf7ccc517e312bf23fa0e07e665aac88
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-09-16 10:01:19 +0400

    Fixed path issues in pymacs-init.el, yet it doesn't function well

--- pymacs-init.el
+++ pymacs-init.el
@@ -1,11 +1,8 @@
-(setq pymacs-dir 
-      (concat 
-       third-party-dir 
-       "bundled/python/pymacs/lib/python"))
 (add-bundled-library-dir "python")
 
 (setq pymacs-load-path 
-      '("/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python/rope-0.9.3-py2.6.egg" 
+      '("/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python" 
+	"/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python/rope-0.9.3-py2.6.egg" 
 	"/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python/ropemacs-0.6-py2.6" 
 	"/Users/serg_mo/.emacs.d/third-party/bundled/python/lib/python/ropemode-0.1_rc2-py2.6"))
 
