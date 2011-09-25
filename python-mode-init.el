f90657aea612af75572cf14855138b682304f014
Sergey Moshnikov <s.moshnikov@gmail.com>
2011-09-25 18:49:51 +0400

    Enabled auto-indent mode for python

--- python-mode-init.el
+++ python-mode-init.el
@@ -4,10 +4,6 @@
   (pymacs-load "ropemacs" "rope-")
   (setq ropemacs-confirm-saving 'nil))
 
-
-(defun install-keybindings ()
-  (define-key python-mode-map "\C-m" 'newline-and-indent))
-
 (defun flymake-create-temp-in-system-tempdir (filename prefix)
   (make-temp-file (or prefix "flymake")))
 
@@ -25,10 +21,12 @@
     (add-to-list 'flymake-allowed-file-name-masks 
 		 '("\\.py\\'" flymake-pyflakes-init))))
 
+(require 'auto-indent)
+
 (add-hook 'python-mode-hook
 	  '(lambda ()
 	     (enable-pyflakes)
 	     (flymake-mode t)
-	     (install-keybindings)))
+	     (auto-indent-mode t)))
 
 
