e8d5db2c795376f7173cdabb6dd5907adadc0e0d
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-02-03 10:42:12 +0400

    Slightly reorganized init order

--- init.el
+++ init.el
@@ -28,9 +28,9 @@
 (add-third-party-dir "bundled")
 
 ;; Customizations for different module
+(load-library "look-and-feel")
 (load-library "intelligent-tab")
 (load-library "yasnippet-init")
-(load-library "look-and-feel")
 (load-library "color-theme-init")
 (load-library "magit-init")
 (load-library "auto-completion-init")
