8df783389d377c5db1d39eec2b9fe7ce6691ce48
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-02-03 10:41:25 +0400

    Refactored python mode init

--- pymacs-init.el
+++ pymacs-init.el
@@ -18,5 +18,3 @@
 (autoload 'pymacs-eval "pymacs" nil t)
 (autoload 'pymacs-exec "pymacs" nil t)
 (autoload 'pymacs-load "pymacs" nil t)
-;;(eval-after-load "pymacs"
-;;  '(add-to-list 'pymacs-load-path pymacs-dir))
--- python-mode-init.el
+++ python-mode-init.el
@@ -10,19 +10,22 @@
 (defun flymake-create-temp-in-system-tempdir (filename prefix)
   (make-temp-file (or prefix "flymake")))
 
-(defun enable-pyflakes ()
-  (when (load "flymake" t) 
-    (defun flymake-pyflakes-init () 
-      ;; Make sure it's not a remote buffer or flymake would not work
-      (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+(defun local-buffer-p ()
+  (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers))))
+
+(defun flymake-pyflakes () 
+      (when (local-buffer-p)
 	(let* ((temp-file (flymake-init-create-temp-buffer-copy 
 			   'flymake-create-temp-in-system-tempdir)) 
 	       (local-file (file-relative-name 
 			    temp-file 
 			    (file-name-directory buffer-file-name)))) 
 	  (list "pyflakes" (list local-file)))))
+
+(defun enable-pyflakes ()
+  (when (load "flymake" t) 
     (add-to-list 'flymake-allowed-file-name-masks 
-		 '("\\.py\\'" flymake-pyflakes-init))))
+		 '("\\.py\\'" flymake-pyflakes))))
 
 (eval-after-load 'python '(load-ropemacs))
 (add-hook 'python-mode-hook
