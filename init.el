addf20953b72a4daa23fd32ace21a78a762b64ca
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2012-06-28 13:06:03 +0400

    dired dwim mode on

--- init.el
+++ init.el
@@ -62,7 +62,8 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(fill-column 80)
- '(truncate-lines t))
+ '(truncate-lines t)
+ '(dired-dwim-target t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
