21a6e2c6b38b2a79a5f0be0c91178256e51c8f4b
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2012-06-29 16:51:35 +0400

    Set tab-width = 2 for Coffee mode

--- coffee-mode-init.el
+++ coffee-mode-init.el
@@ -3,3 +3,8 @@
 (autoload 'coffee-mode "coffee-mode" "Major mode for editing Markdown files")
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(add-hook 'coffee-mode-hook 
+	  '(lambda ()
+	     (make-local-variable 'tab-width)
+	     (set 'tab-width 2)))
