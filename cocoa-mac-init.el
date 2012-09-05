8db9b71361d7e802ef81ebc69f2888a083e61c70
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-09-05 07:01:46 +0400

    Inconsolata font for Mac OS X

--- cocoa-mac-init.el
+++ cocoa-mac-init.el
@@ -1,6 +1,6 @@
 ;; Here I put some Mac-specific Emacs initialization code
 
-(set-face-font 'default "-apple-monaco-medium-r-normal--15-140-72-72-m-140-mac-roman")
+(set-face-font (quote default) "-apple-Inconsolata-medium-normal-normal-*-22-*-*-*-m-0-fontset-auto1")
 
 (blink-cursor-mode t)
 (setq ns-command-modifier 'meta)
