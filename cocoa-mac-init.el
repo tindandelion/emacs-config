30a2144d4bd07b985da774844cede9b0f32cff49
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-09-07 12:25:45 +0400

    Clean up

--- cocoa-mac-init.el
+++
@@ -1,7 +0,0 @@
-;; Here I put some Mac-specific Emacs initialization code
-
-
-;; Maximizing the frame to fit the full screen
-(set-frame-position (selected-frame) 0 0)
-(set-frame-size (selected-frame) 1000 1000)
-
