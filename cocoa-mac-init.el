6671dc697ea519db81921b28d5d63cd860b0bc60
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-09-07 12:25:25 +0400

    Initialization for Mac OS

--- cocoa-mac-init.el
+++ cocoa-mac-init.el
@@ -1,9 +1,5 @@
 ;; Here I put some Mac-specific Emacs initialization code
 
-(set-face-font (quote default) "-apple-Inconsolata-medium-normal-normal-*-22-*-*-*-m-0-fontset-auto1")
-
-(blink-cursor-mode t)
-(setq ns-command-modifier 'meta)
 
 ;; Maximizing the frame to fit the full screen
 (set-frame-position (selected-frame) 0 0)
--- init.el
+++ init.el
@@ -1,2 +1,3 @@
 (load (expand-file-name "packages.el" user-emacs-directory))
 (load (expand-file-name "editor.el" user-emacs-directory))
+(load (expand-file-name "platforms.el" user-emacs-directory))
