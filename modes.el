c67a878fd720641505198907954d2fb349f3370b
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-09-08 12:20:28 +0400

    Markdown mode + proper indentaion of 'use-package' macro

--- modes.el
+++ modes.el
@@ -1,4 +1,15 @@
-(add-hook 'text-mode-hook 
+(defun setup-text-mode ()
+  (add-hook 'text-mode-hook 
 	  '(lambda ()
 	     (setq fill-column 80)
-	     (turn-on-auto-fill)))
+	     (turn-on-auto-fill))))
+
+(defun setup-markdown-mode ()
+  (use-elpa-package 'markdown-mode
+    (add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
+    (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))))
+
+(setup-text-mode)
+(setup-markdown-mode)
+
+
--- packages.el
+++ packages.el
@@ -10,6 +10,7 @@
     (package-install package)))
 
 (defmacro use-elpa-package (name &rest body)
+  (declare (indent defun))
   `(progn
      (ensure-package-installed ,name)
      ,@body))
@@ -17,18 +18,18 @@
 (setup-package-system)
 
 (use-elpa-package 'magit
-		  (global-set-key "\C-xg" 'magit-status))
+  (global-set-key "\C-xg" 'magit-status))
 
 (use-elpa-package 'expand-region
-		  (global-set-key "\M-=" 'er/expand-region))
+  (global-set-key "\M-=" 'er/expand-region))
 
 (use-elpa-package 'rvm)
 
 (use-elpa-package 'smex
-		  (smex-initialize)
-		  (global-set-key "\M-x" 'smex)
-		  (global-set-key (kbd "M-X") 'smex-major-mode-commands))
- 
+  (smex-initialize)
+  (global-set-key "\M-x" 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands))
+
 (use-elpa-package 'full-ack
-		  (global-set-key (kbd "C-c a") 'ack-same)
-		  (global-set-key (kbd "C-c A") 'ack))
+  (global-set-key (kbd "C-c a") 'ack-same)
+  (global-set-key (kbd "C-c A") 'ack))
