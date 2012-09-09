d487916ded40229afec3a43e40f0d59da153a057
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-09-09 12:18:12 +0400

    Added || to electric braces

--- modes.el
+++ modes.el
@@ -1,15 +1,23 @@
-(defun setup-text-mode ()
-  (add-hook 'text-mode-hook 
-	  '(lambda ()
-	     (setq fill-column 80)
-	     (turn-on-auto-fill))))
+(defmacro setup-mode (mode-name &rest body)
+  (declare (indent defun))
+  `(progn ,@body))
 
-(defun setup-markdown-mode ()
+(setup-mode 'text-mode
+  (add-hook 'text-mode-hook 
+	    '(lambda ()
+	       (setq fill-column 80)
+	       (turn-on-auto-fill))))
+
+(setup-mode 'markdown-mode
   (use-elpa-package 'markdown-mode
     (add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
     (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))))
 
-(setup-text-mode)
-(setup-markdown-mode)
-
-
+(setup-mode 'ruby-mode
+  (use-elpa-package 'ruby-mode)
+  (add-hook 'ruby-mode-hook
+	    '(lambda ()
+	       (use-elpa-package 'rvm)
+	       (rvm-activate-corresponding-ruby)
+	       (add-to-list (make-local-variable 'electric-pair-pairs)
+			    '(?| . ?|)))))
