2811cacaac1d8386c03aae941228f1952fd858f9
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-03-30 07:29:25 +0400

    Removed cucumber-init.el

--- cucumber-init.el
+++
@@ -1,6 +0,0 @@
-(add-github-library-dir "cucumber")
-
-(require 'feature-mode)
-(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
-(yas/load-directory (make-third-party-dir "github/cucumber/snippets"))
-
